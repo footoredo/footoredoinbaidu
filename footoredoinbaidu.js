dd1eb637dd8110c69a418af96040d31483c3b1d4
footoredo <footoredo@gmail.com>
2015-08-07 22:24:26 +0800

    Mostly done

--- footoredoinbaidu.js
+++ footoredoinbaidu.js
@@ -1,1 +1,31 @@
-const http = require('')
+const http = require('http')
+const url = require('url')
+
+var site = "http://footoredo.in"
+
+var server = http.createServer(function(req, res) {
+    if (req.method != 'GET')
+      res.end('Oops!')
+
+    var options = {
+      hostname: site,
+      port: 80,
+      path: url.parse(req.url, true).path,
+      method: 'GET'
+    }
+    console.log(options)
+
+    http.get(site + url.parse(req.url, true).path, function(_res) {
+        //_res.pipe(res) 
+        _res.on('data', function(chunk) {
+          res.write(chunk)
+          })
+        _res.on('end', function() {
+          res.end()
+          })
+      }).on('error',function(e) {
+        console.log("Got error: " + e.message);
+        })
+    
+    })
+server.listen(8000)
